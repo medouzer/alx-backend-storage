040f5f7d5614d15d63b324744f1b8675f55b1887
ouzerzou <mohamed.ouz2016@gmail.com>
2024-05-14 22:12:16 +0100

    fix error

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -10,20 +10,20 @@ def show_logs():
     nginx_collection = client.logs.nginx
 
     total_logs = nginx_collection.count_documents({})
-    print(total_logs)
-    print("Methods:")
     get_logs = nginx_collection.count_documents({"method": "GET"})
-    print(f"\tmethod GET: {get_logs}")
     post_logs = nginx_collection.count_documents({"method": "POST"})
-    print(f"\tmethod POST: {post_logs}")
     put_logs = nginx_collection.count_documents({"method": "PUT"})
-    print(f"\tmethod PUT: {put_logs}")
     patch_logs = nginx_collection.count_documents({"method": "PATCH"})
-    print(f"\tmethod PATCH: {patch_logs}")
     delete_logs = nginx_collection.count_documents({"method": "DELETE"})
-    print(f"\tmethod DELETE: {delete_logs}")
     get_status_logs = nginx_collection.count_documents(
         {"method": "GET", "path": "/status"})
+    print(total_logs)
+    print("Methods:")
+    print(f"\tmethod GET: {get_logs}")
+    print(f"\tmethod POST: {post_logs}")
+    print(f"\tmethod PUT: {put_logs}")
+    print(f"\tmethod PATCH: {patch_logs}")
+    print(f"\tmethod DELETE: {delete_logs}")
     print(f"{get_status_logs} status check")
 
 if __name__ == "__main__":
