b28df58af4aba7350637cf2de452b309d685c25c
mohamed <mouhamed.ouz2016@gmail.com>
2024-05-14 14:02:50 -0700

    new things

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Log stats"""
+
+from pymongo import MongoClient
+
+
+def show_logs():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+
+    total_logs = nginx_collection.count_documents({})
+    print(total_logs)
+    print("Methods:")
+    get_logs = nginx_collection.count_documents({"method": "GET"})
+    print(f"\tmethod GET: {get_logs}")
+    post_logs = nginx_collection.count_documents({"method": "POST"})
+    print(f"\tmethod POST: {post_logs}")
+    put_logs = nginx_collection.count_documents({"method": "PUT"})
+    print(f"\tmethod PUT: {put_logs}")
+    patch_logs = nginx_collection.count_documents({"method": "PATCH"})
+    print(f"\tmethod PATCH: {patch_logs}")
+    delete_logs = nginx_collection.count_documents({"method": "DELETE"})
+    print(f"\tmethod DELETE: {delete_logs}")
+    get_status_logs = nginx_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{get_status_logs} status check")
+
+if __name__ == "__main__":
+    show_logs()
